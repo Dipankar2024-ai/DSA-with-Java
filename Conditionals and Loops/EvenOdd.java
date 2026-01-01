80f3711b61302e5a126056b5089b41ed47fccd1d
Dipankar2024-ai <166995389+Dipankar2024-ai@users.noreply.github.com>
2026-01-01 12:57:39 +0530

    Create EvenOdd.java

---
+++ Conditionals and Loops/EvenOdd.java
@@ -0,0 +1,22 @@
+package ConditionalsAndLoops;
+
+import java.util.Scanner;
+
+public class EvenODD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n%2==0)
+		{
+			System.out.println("It is a Even Number");
+		}
+		else {
+			System.out.println("It is a Odd Number");
+		}
+		
+
+	}
+
+}
